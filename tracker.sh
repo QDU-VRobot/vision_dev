b23185c80a4811eef3c0172de76297dd8e438346
find <1924192427@qq.com>
2026-03-09 22:46:21 +0800

    修正启动脚本

--- tracker.sh
+++ tracker.sh
@@ -1,22 +1,26 @@
+# 设置开机自启
+# 在终端中输入 gnome-session-properties
+# 在弹出的“启动应用程序首选项”窗口中，点击“添加”，输入名称、命令和注释，然后点击“添加”完成设置。
+
 #!/bin/bash
 
 delay_time=3
 while true
 do
-    ps -ef | grep "armor_tracker_node" | grep -v "grep"
-    if ["$?" -ne 0]
+    ps -ef | grep "armor_tracker_node" | grep -v "grep" > /dev/null
+    if [ $? -ne 0 ]
     then
-    sleep $delay_time
-        #gnome-terminal -t "tracker" -x bash -c "cd /home/rm/tracker;source install/setup.bash;  ros2 launch rm_vision_bringup vision_bringup.launch.py;exec bash;"
-        konsole -e bash -c 'cd /home/rm/tracker;source install/setup.bash;  ros2 launch rm_vision_bringup vision_bringup.launch.py'
-        if [$? -eq 0]
+        echo "tracker未运行，等待 ${delay_time}秒 后启动..."
+        sleep $delay_time
+         gnome-terminal --title="tracker" -- bash -c "cd /home/find/code/vision_/vision_dev/ && source install/setup.bash && ros2 launch rm_vision_bringup vision_bringup.launch.py; exec bash"
+        if [ $? -eq 0 ]
         then
             echo "tracker started successfully!"
         else
             echo "tracker failed to start!"
         fi
     else
-    echo "tracker is running!"
+        echo "tracker is running!"
     fi
     sleep $delay_time
 done
